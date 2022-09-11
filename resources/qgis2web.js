443cede62b55abbaf3e44569f2a9b7c6aa646de6
LabHidrFRCUTN <100687868+LabHidrFRCUTN@users.noreply.github.com>
2022-09-11 20:59:47 -0300

    Update qgis2web.js

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -609,12 +609,15 @@ map.on("rendercomplete", function(evt) {
         var attribution = document.getElementsByClassName('ol-attribution')[0];
         var attributionList = attribution.getElementsByTagName('ul')[0];
         var firstLayerAttribution = attributionList.getElementsByTagName('li')[0];
-        var qgis2webAttribution = document.createElement('li');
+        var utnlabwebAttribution = document.createElement('li');
+        utnlabwebAttribution.innerHTML = '<a href="https://www.investigacion.frc.utn.edu.ar/hidraulica/">UTN FRC Laboratorio de Hidraulica</a> &middot; ';
+	   var qgis2webAttribution = document.createElement('li');
         qgis2webAttribution.innerHTML = '<a href="https://github.com/tomchadwin/qgis2web">qgis2web</a> &middot; ';
         var olAttribution = document.createElement('li');
         olAttribution.innerHTML = '<a href="https://openlayers.org/">OpenLayers</a> &middot; ';
         var qgisAttribution = document.createElement('li');
         qgisAttribution.innerHTML = '<a href="https://qgis.org/">QGIS</a>';
+   attributionList.insertBefore(utnlabwebAttribution, firstLayerAttribution);
         attributionList.insertBefore(qgis2webAttribution, firstLayerAttribution);
         attributionList.insertBefore(olAttribution, firstLayerAttribution);
         attributionList.insertBefore(qgisAttribution, firstLayerAttribution);
